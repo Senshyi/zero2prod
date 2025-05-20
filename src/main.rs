c319dd5a05142484956f8d7816dbb10b1518e16f
Jan Machacek <janmachacek97@gmail.com>
2025-05-20 19:23:24 +0200

    health check with simple test and organization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:7676").expect("Failed to bind port 7676");
+    run(listener)?.await
 }
